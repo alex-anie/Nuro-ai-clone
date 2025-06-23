4b5c0fc4136b8a63b7bddd8ca75ea2778557f8e3
alex-anie <ocxigin@gmail.com>
2025-06-23 15:51:25 +0100

    Navbar completed

--- views/home.php
+++ views/home.php
@@ -1,2 +1,12 @@
-<?php require('partials/navbar.php')?>
+<main class="w-[95%] mx-auto mt-0 md:mt-10">
+    <section>
+        <?php require('partials/navbar.php')?>
+    </section>
 
+    <section class="h-200 w-full md:h-160 rounded-3xl">
+        <video autoplay preload loop muted playsinline class="h-full w-full rounded-3xl object-cover object-[50%_50%]">
+            <source src="/assets/videos/home-drive.mp4" type="video/mp4"/>
+        </video>
+    </section>
+
+</main>
