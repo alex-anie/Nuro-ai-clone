c525a1ebe30294760899374abbebbbeb3f9c2bdc
alex-anie <ocxigin@gmail.com>
2025-06-24 13:38:44 +0100

    Play button styles added

--- public/index.php
+++ public/index.php
@@ -6,6 +6,7 @@
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     <link href="/css/styles.css" rel="stylesheet">
     <link href="/css/main.css" rel="stylesheet">
+    <title>Nuro - Autonomy for all. All roads, all rides. | Nuro</title>
   </head>
   <body>
       <?php require('../router.php'); ?>
--- views/home.php
+++ views/home.php
@@ -3,10 +3,30 @@
         <?php require('partials/navbar.php')?>
     </section>
 
-    <section class="h-200 w-full md:h-160 rounded-3xl">
+    <section class="h-200 w-full md:h-160 rounded-3xl relative">
         <video autoplay preload loop muted playsinline class="h-full w-full rounded-3xl object-cover object-[50%_50%]">
             <source src="/assets/videos/home-drive.mp4" type="video/mp4"/>
         </video>
+
+        <section>
+            <aside class="absolute top-8 left-5 md:top-1/2 md:-translate-y-1/2">
+                <h1 class="text-white text-5xl" style="font-family: SuisseIntl; font-weight: 400;">Autonomy for all. <br />All roads, all rides.</h1>
+                <p class="text-white text-2xl mt-4" style="font-family: SuisseIntl; font-weight: 200;">Building the most scalable AI driver <br /> for automotive and mobility applications.</p>
+
+                <button class="relative flex cursor-pointer mt-8">
+                    <span class="absolute inline-flex h-full w-full animate-ping border border-white rounded-full  opacity-75 [animation-delay:2.1s] [animation-duration:3.4s]"></span>
+                    <span class="absolute inline-flex h-full w-full animate-ping border border-white bg-white rounded-full  opacity-75 [animation-delay:1.6s] [animation-duration:2.4s]"></span>
+                    <span class="absolute inline-flex h-full w-full animate-ping border border-white bg-white rounded-full  opacity-75 [animation-delay:3.6s] [animation-duration:3.4s]"></span>
+                    <svg width="15" height="15" class="border border-white size-6 md:size-12 rounded-full text-white" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M6 11L6 4L10.5 7.5L6 11Z" fill="currentColor"></path>
+                    </svg>
+                </button>
+        </aside>
+
+            <aside>
+
+            </aside>
+        </section>
     </section>
 
 </main>
