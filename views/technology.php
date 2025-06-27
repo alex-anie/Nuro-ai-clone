aa27946801f3d271275641cdd63adb943b1d4529
alex-anie <ocxigin@gmail.com>
2025-06-27 09:28:36 +0100

    visual section

--- views/technology.php
+++ views/technology.php
@@ -9,7 +9,7 @@
     }
 </style>
 <div id="site-wrapper" class="bg-neutral-800">
-<main class="w-[95%] mx-auto mt-0 md:pt-10 " >
+<main class="w-[95%] mx-auto mt-0 md:pt-10">
     <!-- Navbar -->
     <section>
         <?php require('partials/navbar.php')?>
@@ -116,13 +116,65 @@
     <!-- Nuro's Autonomous Driving system -->
      <section class="mt-18">
         <h1 
-        class="text-4xl lg:text-5xl w-full md:w-[60%]"
+        class="text-white text-4xl lg:text-5xl w-full md:w-[60%]"
         style="
         font-family: SuisseIntl;
         font-weight: 500;
         ">
             Nuro's Autonomous Driving system.
         </h1>
+
+        <section>
+            <article class="grid lg:grid-cols-2 gap-8 lg:gap-y-0 mt-12 md:w-full">
+                <aside>
+                    <div class="relative w-full h-160 md:h-[20rem] lg:h-[25rem] xl:h-[30rem] rounded-3xl">
+                        <img class="h-full w-full rounded-3xl object-cover" src="/assets/images/technology/desing-scale-nuro-car.png" alt="nuro car passenger-mobility">
+                    
+
+                    <button class="group hover:bg-sky-600 flex gap-x-4 justify-between items-center absolute bottom-8 left-6 border border-sky-600 rounded-xl py-2 px-3" style="transition: background 0.5s linear">
+                            <span class="text-sky-600 group-hover:text-white" style="font-family: SuisseIntlMono; font-weight: 200;">See the components</span>
+                            <svg class="size-[18px] rounded-full border border-sky-600 stroke-sky-600 group-hover:stroke-white group-hover:border-white" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8 2.75C8 2.47386 7.77614 2.25 7.5 2.25C7.22386 2.25 7 2.47386 7 2.75V7H2.75C2.47386 7 2.25 7.22386 2.25 7.5C2.25 7.77614 2.47386 8 2.75 8H7V12.25C7 12.5261 7.22386 12.75 7.5 12.75C7.77614 12.75 8 12.5261 8 12.25V8H12.25C12.5261 8 12.75 7.77614 12.75 7.5C12.75 7.22386 12.5261 7 12.25 7H8V2.75Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
+                        </button>
+                    </div>
+
+                    <div class="p-6">
+                        <p class="text-white text-2xl mt-3" style="font-family: SuisseIntl; font-weight: 200;">The Nuro Driver™</p>
+                        
+                        <p class="text-neutral-400" style="font-family: SuisseIntl; font-weight: 200;">
+                        In-vehicle software and hardware to enable automated driving functionality.
+                        </p>
+                    </div>
+
+                    <div class="pl-6 pt-2">
+                        <a class="bg-white text-black py-2 px-4 rounded-full inline-block hover:text-white hover:bg-sky-700 transition-colors" style="font-family: SuisseIntl; font-weight: 200;" href="/nuro-driver">Learn More</a>
+                    </div>
+                </aside>
+
+                <aside>
+                    <div class="relative w-full h-160 md:h-[20rem] lg:h-[25rem] xl:h-[30rem] rounded-3xl">
+                        <img class="h-full w-full rounded-3xl object-cover" src="/assets/images/technology/Nuro-AI-Platform_NuroSensorVehicle.png" alt="nuro car passenger-mobility">
+                    
+                    <button class="group hover:bg-sky-600 flex gap-x-4 justify-between items-center absolute bottom-8 left-6 border border-sky-600 rounded-xl py-2 px-3" style="transition: background 0.5s linear">
+                            <span class="text-sky-600 group-hover:text-white" style="font-family: SuisseIntlMono; font-weight: 200;">See the components</span>
+                            <svg class="size-[18px] rounded-full border border-sky-600 stroke-sky-600 group-hover:stroke-white group-hover:border-white" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8 2.75C8 2.47386 7.77614 2.25 7.5 2.25C7.22386 2.25 7 2.47386 7 2.75V7H2.75C2.47386 7 2.25 7.22386 2.25 7.5C2.25 7.77614 2.47386 8 2.75 8H7V12.25C7 12.5261 7.22386 12.75 7.5 12.75C7.77614 12.75 8 12.5261 8 12.25V8H12.25C12.5261 8 12.75 7.77614 12.75 7.5C12.75 7.22386 12.5261 7 12.25 7H8V2.75Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
+                    </button>
+                    </div>
+
+                    <div class="p-6">
+                        <p class="text-white text-2xl mt-3" style="font-family: SuisseIntl; font-weight: 200;">The Nuro AI Platform™</p>
+                        
+                        <p class="text-neutral-400" style="font-family: SuisseIntl; font-weight: 200;">
+                        Industry-leading autonomy developer tools, infrastructure, and remote monitoring.
+                        </p>
+                    </div>
+
+                    <div class="pl-6 pt-2">
+                        <a class="bg-white text-black py-2 px-4 rounded-full inline-block hover:text-white hover:bg-sky-700 transition-colors" style="font-family: SuisseIntl; font-weight: 200;" href="/nuro-ai-platform">Learn More</a>
+                    </div>
+                </aside>
+            
+        </article>
+    </section>
     </section>
 
     <!-- Safety at every turn section -->
