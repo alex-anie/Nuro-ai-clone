ddcc62f14a28e123044454ffb32d3d33ed83c4a7
alex-anie <ocxigin@gmail.com>
2025-06-28 17:28:20 +0100

    company leader

--- views/company.php
+++ views/company.php
@@ -174,51 +174,65 @@
         font-family: SuisseIntl;
         font-weight: 500;
         ">
-            Our founders.
+            Company leadership.
         </h1>
 
         <section>
-            <article class="grid lg:grid-cols-2 gap-8 lg:gap-y-0 mt-12 md:w-full">
+            <article class="grid md:grid-cols-3 lg:grid-cols-5 gap-8 lg:gap-y-0 mt-12 md:w-full">
                 <aside>
                     <div class="relative w-full h-160 md:h-[20rem] lg:h-[25rem] xl:h-[30rem] rounded-3xl">
-                        <img class="h-full w-full rounded-3xl object-cover" src="/assets/images/company/Jiajun-Zhu.png" alt="nuro car passenger-mobility">
-
-                        <a href="https://www.linkedin.com/in/jiajun/" class="group hover:bg-sky-600 flex gap-x-4 justify-between items-center absolute bottom-8 left-6 border border-sky-600 rounded-xl py-2 px-3" style="transition: background 0.5s linear">
-                            <span class="text-sky-600 group-hover:text-white" style="font-family: SuisseIntlMono; font-weight: 200;">Follow on Linkedin</span>
-                            <svg class="size-[18px] rounded-full border border-sky-600 stroke-sky-600 group-hover:stroke-white group-hover:border-white" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8 2.75C8 2.47386 7.77614 2.25 7.5 2.25C7.22386 2.25 7 2.47386 7 2.75V7H2.75C2.47386 7 2.25 7.22386 2.25 7.5C2.25 7.77614 2.47386 8 2.75 8H7V12.25C7 12.5261 7.22386 12.75 7.5 12.75C7.77614 12.75 8 12.5261 8 12.25V8H12.25C12.5261 8 12.75 7.77614 12.75 7.5C12.75 7.22386 12.5261 7 12.25 7H8V2.75Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
-                        </a>
+                        <img class="h-full w-full rounded-3xl object-cover" src="/assets/images/company/andrew-chapin.png" alt="nuro car passenger-mobility">
                     </div>
 
                     <div class="p-6">
-                        <p class="text-2xl mt-3" style="font-family: SuisseIntl; font-weight: 200;">Jiajun Zhu</p>
-
-                        <p class="text-blue-800 text-[12px] my-3" style="font-family: SuisseIntl; font-weight: 200;">Co-Founder and CEO</p>
+                        <p class="text-2xl mt-3" style="font-family: SuisseIntl; font-weight: 200;">Andrew Chapin</p>
+                        <p class="text-blue-800 text-[12px] my-3" style="font-family: SuisseIntl; font-weight: 200;">Chief Operating Officer</p>
                         
-                        <p class="text-neutral-400" style="font-family: SuisseIntl; font-weight: 200;">
-                        Prior to founding Nuro, Jiajun was a principal software engineer at Google and one of the founding team members of the company’s self-driving program—now known as Waymo. In addition to leading the project’s perception team, he also helped build and lead their simulation efforts. Jiajun earned his Master’s and Bachelor’s degree in computer science from the University of Virginia and Fudan University respectively. Jiajun has been awarded over 100 patents.
-                        </p>
                     </div>
                 </aside>
 
                 <aside>
                     <div class="relative w-full h-160 md:h-[20rem] lg:h-[25rem] xl:h-[30rem] rounded-3xl">
-                        <img class="h-full w-full rounded-3xl object-cover" src="/assets/images/company/dave-ferguson.png" alt="nuro car passenger-mobility">
-                    
-                    <a href="https://www.linkedin.com/in/dave-ferguson" class="group hover:bg-sky-600 flex gap-x-4 justify-between items-center absolute bottom-8 left-6 border border-sky-600 rounded-xl py-2 px-3" style="transition: background 0.5s linear">
-                            <span class="text-sky-600 group-hover:text-white" style="font-family: SuisseIntlMono; font-weight: 200;">Follow on Linkedin</span>
-                            <svg class="size-[18px] rounded-full border border-sky-600 stroke-sky-600 group-hover:stroke-white group-hover:border-white" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8 2.75C8 2.47386 7.77614 2.25 7.5 2.25C7.22386 2.25 7 2.47386 7 2.75V7H2.75C2.47386 7 2.25 7.22386 2.25 7.5C2.25 7.77614 2.47386 8 2.75 8H7V12.25C7 12.5261 7.22386 12.75 7.5 12.75C7.77614 12.75 8 12.5261 8 12.25V8H12.25C12.5261 8 12.75 7.77614 12.75 7.5C12.75 7.22386 12.5261 7 12.25 7H8V2.75Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
-                        </a>
+                        <img class="h-full w-full rounded-3xl object-cover" src="/assets/images/company/james-owen.png" alt="nuro car passenger-mobility">
                     </div>
 
                     <div class="p-6">
-                        <p class="text-2xl mt-3" style="font-family: SuisseIntl; font-weight: 200;">Dave Ferguson</p>
-
-                        <p class="text-blue-800 text-[12px] my-3" style="font-family: SuisseIntl; font-weight: 200;">Co-Founder and President</p>
+                        <p class="text-2xl mt-3" style="font-family: SuisseIntl; font-weight: 200;">James Owen</p>
+                        <p class="text-blue-800 text-[12px] my-3" style="font-family: SuisseIntl; font-weight: 200;">Chief Legal Officer</p>
                         
-                        <p class="text-neutral-400" style="font-family: SuisseIntl; font-weight: 200;">
-                       Prior to founding Nuro, Dave was a principal engineer on Google’s self-driving program, now known as Waymo. He built and led the computer vision, machine learning, behavior prediction, and scene understanding teams. He also led the planning group for Carnegie Mellon University's team that won the DARPA Urban Grand Challenge in 2007. Dave holds an MS and PhD in Robotics from Carnegie Mellon and a Bachelor’s in CS and Mathematics from the University of Otago. Dave holds over 100 patents and has published over 60 academic papers.
-                        </p>
                     </div>
+                </aside>
+
+                <aside>
+                    <div class="relative w-full h-160 md:h-[20rem] lg:h-[25rem] xl:h-[30rem] rounded-3xl">
+                        <img class="h-full w-full rounded-3xl object-cover" src="/assets/images/company/jack-guo.png" alt="nuro car passenger-mobility">
+                    </div>
+
+                    <div class="p-6">
+                        <p class="text-2xl mt-3" style="font-family: SuisseIntl; font-weight: 200;">Jack Guo</p>
+                        <p class="text-blue-800 text-[12px] my-3" style="font-family: SuisseIntl; font-weight: 200;">VP of Engineer, AI Platform</p>
+                    </div>
+                </aside>
+
+                 <aside>
+                    <div class="relative w-full h-160 md:h-[20rem] lg:h-[25rem] xl:h-[30rem] rounded-3xl">
+                        <img class="h-full w-full rounded-3xl object-cover" src="/assets/images/company/Tilo-Schwarz.png" alt="nuro car passenger-mobility">
+                    </div>
+
+                    <div class="p-6">
+                        <p class="text-2xl mt-3" style="font-family: SuisseIntl; font-weight: 200;">Tilo Schwarz</p>
+                        <p class="text-blue-800 text-[12px] my-3" style="font-family: SuisseIntl; font-weight: 200;">VP of Engineer, Vehicle Platform & Hardwars</p>
+                    </div>
+                </aside>
+
+                 <aside>
+                    <div class="relative w-full h-160 md:h-[20rem] lg:h-[25rem] xl:h-[30rem] rounded-3xl">
+                        <img class="h-full w-full rounded-3xl object-cover" src="/assets/images/company/matt-renna.png" alt="nuro car passenger-mobility">
+                    </div>
+
+                    <div class="p-6">
+                        <p class="text-2xl mt-3" style="font-family: SuisseIntl; font-weight: 200;">Matt Renna</p>
+                        <p class="text-blue-800 text-[12px] my-3" style="font-family: SuisseIntl; font-weight: 200;">VP of Engineer, Vehicle Platform & Hardwars</p>
                     </div>
                 </aside>
             
