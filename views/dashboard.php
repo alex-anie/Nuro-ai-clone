5e3ffb396918b34b094d0b2b6d7643f4bdc362d1
alex-anie <ocxigin@gmail.com>
2025-06-30 21:52:09 +0100

    sidenav added

--- views/dashboard.php
+++ views/dashboard.php
@@ -1,29 +1,18 @@
 <main>
-    <nav class="h-15 w-full shadow-xl flex justify-between items-center px-10">
-       <div>
-           <button>
-                <svg class="w-[30px] h-[30px] cursor-pointer text-neutral-600 hover:text-sky-500" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.84182 3.13514C9.04327 3.32401 9.05348 3.64042 8.86462 3.84188L5.43521 7.49991L8.86462 11.1579C9.05348 11.3594 9.04327 11.6758 8.84182 11.8647C8.64036 12.0535 8.32394 12.0433 8.13508 11.8419L4.38508 7.84188C4.20477 7.64955 4.20477 7.35027 4.38508 7.15794L8.13508 3.15794C8.32394 2.95648 8.64036 2.94628 8.84182 3.13514Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
-        </button>
-       </div>
+    <section>
+        <?php require('partials/dashboardnav.php')?>
+    </section>
 
-        <aside class="">
-            <form action="" method="post">
-                <div class="md:w-100 lg:w-160 h-full flex gap-1 bg-neutral-100 rounded-xl px-2 py-1 focus-within:outline focus-within:outline-sky-500" style="transition: background 0.2s linear;">
-                    <svg  class="w-[30px] h-[30px] cursor-pointer text-neutral-600" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10 6.5C10 8.433 8.433 10 6.5 10C4.567 10 3 8.433 3 6.5C3 4.567 4.567 3 6.5 3C8.433 3 10 4.567 10 6.5ZM9.30884 10.0159C8.53901 10.6318 7.56251 11 6.5 11C4.01472 11 2 8.98528 2 6.5C2 4.01472 4.01472 2 6.5 2C8.98528 2 11 4.01472 11 6.5C11 7.56251 10.6318 8.53901 10.0159 9.30884L12.8536 12.1464C13.0488 12.3417 13.0488 12.6583 12.8536 12.8536C12.6583 13.0488 12.3417 13.0488 12.1464 12.8536L9.30884 10.0159Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
+    <section>
+        <article>
+            <aside>
+                <?php require('partials/sidenav.php') ?>
+            </aside>
 
-                    <input class="w-full outline-0 border-0" placeholder="search..." type="text" name="footer-submit" type="search" id="footer-submit" style="font-family: SuisseIntl; font-weight: 200;">
-                    <button class="">
-                        <svg class="w-[20px] h-[20px] cursor-pointer text-neutral-600 hover:text-sky-500" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1.20308 1.04312C1.00481 0.954998 0.772341 1.0048 0.627577 1.16641C0.482813 1.32802 0.458794 1.56455 0.568117 1.75196L3.92115 7.50002L0.568117 13.2481C0.458794 13.4355 0.482813 13.672 0.627577 13.8336C0.772341 13.9952 1.00481 14.045 1.20308 13.9569L14.7031 7.95693C14.8836 7.87668 15 7.69762 15 7.50002C15 7.30243 14.8836 7.12337 14.7031 7.04312L1.20308 1.04312ZM4.84553 7.10002L2.21234 2.586L13.2689 7.50002L2.21234 12.414L4.84552 7.90002H9C9.22092 7.90002 9.4 7.72094 9.4 7.50002C9.4 7.27911 9.22092 7.10002 9 7.10002H4.84553Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
-                    </button>
-                </div>
-            </form>
-        </aside>
+            <aside>
 
-        <aside>
-            <div class="size-[30px] rounded-full border border-sky-600 p-[2px]">
-                <img src="assets/images/dashboard/lucas-grayman.jpg" alt="" class="size-full rounded-full object-cover object-center">
-            </div>
-        </aside>
-    </nav>
+            </aside>
+        </article>
+    </section>
 
 </main>
