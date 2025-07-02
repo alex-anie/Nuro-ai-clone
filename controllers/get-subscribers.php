56a15166890fa4ba089811fd34a437308ec96308
alex-anie <ocxigin@gmail.com>
2025-07-02 21:35:33 +0100

    Pagination is fixed

--- controllers/get-subscribers.php
+++ controllers/get-subscribers.php
@@ -2,17 +2,18 @@
 
 require_once '../models/Database.php';
 
-$start = 0;
 $page_limit = 4;
+$page = isset($_GET['page_number']) ? (int)$_GET['page_number'] - 1 : 0;
+$start = $page * $page_limit;
 
 $db = new Database();
 
-// Select all subscribers from the table
+//Get subscribers for current page
 $subscribers = $db->fetchAll("SELECT * FROM `subscribers` ORDER BY `id` DESC LIMIT $start, $page_limit");
 
 
-//Pagination
+//Get total Count
 $all_subs = $db->fetchAll("SELECT * FROM `subscribers`");
 $number_of_subs = count($all_subs);
-$number_of_pages = $number_of_subs / $page_limit;
+$number_of_pages = ceil($number_of_subs / $page_limit);
 
